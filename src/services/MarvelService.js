c9eec440327ee303f6ef52bf1e6cb195815b21b0
Erik Kopcha <erickopcha@gmail.com>
2022-01-07 18:02:12 +0200

    update: service, random char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,7 @@ class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=b8edc1e2fb6943c2772b745402fa988d';
 
-  async getResource(url) {
+  getResource = async url => {
     let res = await fetch(url);
 
     if (!res.ok) {
@@ -10,17 +10,38 @@ class MarvelService {
     }
 
     return await res.json();
-  }
+  };
 
-  getAllCharacters() {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`,
     );
-  }
 
-  getCharacter(id) {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-  }
+    return res.data.results.map(this._transformCharacter);
+  };
+
+  getCharacter = async id => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`,
+    );
+
+    return this._transformCharacter(res);
+  };
+
+  _transformCharacter = res => {
+    const { name, description, thumbnail, urls } = res.data.results[0];
+
+    if (!res.data.results[0])
+      return console.error('_transformCharacter: result is not defined');
+
+    return {
+      name: name,
+      description: description,
+      thumbnail: `${thumbnail.path}.${thumbnail.extension}`,
+      homepage: urls[0].url,
+      wiki: urls[1].url,
+    };
+  };
 }
 
 export default MarvelService;
