5fd33793374cf877718dd0c8e1a7f2bab8140f68
Erik Kopcha <erickopcha@gmail.com>
2022-01-18 08:23:13 +0200

    update: types

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -2,7 +2,7 @@ import './spinner.scss';
 
 const Spinner = () => {
   return (
-    <div class="spinner-container">
+    <div className="spinner-container">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         style={{ margin: '0 auto', background: 'none', display: 'block' }}
