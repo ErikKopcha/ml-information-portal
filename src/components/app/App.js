bbea1b6e2800b600d1b37b0e8010825d050e612f
Erik Kopcha <erickopcha@gmail.com>
2022-06-21 20:37:39 +0300

    feat: dynamic page import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useState, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AppHeader from '../appHeader/AppHeader';
-import { HomePage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(() => import('../pages/404/404'));
+const HomePage = lazy(() => import('../pages/home/HomePage'));
+const ComicsPage = lazy(() => import('../pages/comics/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/singleComic/SingleComicPage'));
 
 const App = () => {
   const [selectedChar, setChar] = useState(null);
@@ -16,20 +22,22 @@ const App = () => {
         <AppHeader />
 
         <main>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <HomePage
-                  selectedChar={selectedChar}
-                  onCharSelected={id => onCharSelected(id)}
-                />
-              }
-            />
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <HomePage
+                    selectedChar={selectedChar}
+                    onCharSelected={id => onCharSelected(id)}
+                  />
+                }
+              />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </Router>
